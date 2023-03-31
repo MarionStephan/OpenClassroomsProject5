693151387d895bb2614ca2ff393324a4088b7e98
MarionStephan <106662880+MarionStephan@users.noreply.github.com>
2023-03-31 14:20:58 +0200

    Ajout fonction suppression et modification panier

---
+++ front/js/cart.js
@@ -0,0 +1,98 @@
+const panier = JSON.parse(localStorage.getItem("kanapLs"));
+
+async function fetchApi() {
+  let tableauPanier = [];
+  if (panier !== null) {
+    for (let i = 0; i < panier.length; i++) {
+      await fetch("http://localhost:3000/api/products/" + panier[i].id)
+        .then((res) => res.json())
+        .then((kanap) => {
+          const produit = {
+            _id: kanap._id,
+            nom: kanap.name,
+            prix: kanap.price,
+            couleur: panier[i].colors,
+            quantite: panier[i].quantity,
+            alt: kanap.altTxt,
+            img: kanap.imageUrl,
+          };
+          tableauPanier.push(produit);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    }
+  }
+  return tableauPanier;
+}
+
+async function affichagePanier() {
+  const produits = await fetchApi();
+  if (panier !== null && panier.length !== 0) {
+    const placePanier = document.getElementById("cart__items");
+    produits.forEach((produit) => {
+      placePanier.insertAdjacentHTML(
+        "beforeend",
+        `<article class="cart__item" data-id="${produit._id}" data-color="${produit.couleur}">
+        <div class="cart__item__img">
+          <img src="${produit.img}" alt="${produit.altTxt}">
+        </div>
+        <div class="cart__item__content">
+          <div class="cart__item__content__description">
+            <h2>${produit.nom}</h2>
+            <p>${produit.couleur}</p>
+            <p>${produit.prix} €</p>
+          </div>
+          <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+              <p>Qté : </p>
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${produit.quantite}">
+            </div>
+            <div class="cart__item__content__settings__delete">
+              <p class="deleteItem">Supprimer</p>
+            </div>
+          </div>
+        </div>
+      </article>`
+      );
+    });
+  }
+}
+function recupPanier() {
+  return JSON.parse(localStorage.getItem("kanapLs"));
+}
+function sauvPanier(panier) {
+  localStorage.setItem("kanapLs", JSON.stringify(panier));
+}
+function modifQte(id, color, nouvQte) {
+  const panier = recupPanier();
+  for (let i in panier) {
+    if (panier[i].id === id && panier[i].colors === color) {
+      panier[i].quantity = nouvQte;
+      sauvPanier(panier);
+    }
+  }
+}
+document.addEventListener("change", (event) => {
+  if (event.target.classList.contains("itemQuantity")) {
+    const id = event.target.closest(".cart__item").dataset.id;
+    const color = event.target.closest(".cart__item").dataset.color;
+    const nouvQte = event.target.value;
+    modifQte(id, color, nouvQte);
+    location.reload(); // recharge la page pour mettre à jour l'affichage du panier
+  }
+});
+function supprimerProduit(id, color) {
+  let panier = recupPanier();
+  panier = panier.filter((produit) => produit.id !== id || produit.colors !== color);
+  sauvPanier(panier);
+  location.reload(); // recharge la page pour mettre à jour l'affichage du panier
+}
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("deleteItem")) {
+    const id = event.target.closest(".cart__item").dataset.id;
+    const couleur = event.target.closest(".cart__item").dataset.color;
+    supprimerProduit(id, couleur);
+  }
+});
+affichagePanier();
--- front/js/product.js
+++ front/js/product.js
@@ -1,26 +1,86 @@
 const urlParams = new URLSearchParams(document.location.search);
 const kanapId = urlParams.get("id");
 const produitUrl = `http://localhost:3000/api/products/${kanapId}`;
-
-async function getProduit() {
-  return fetch(produitUrl)
-    .then((res) => res.json())
-    .catch((err) => console.log(err));
-}
-
-async function displayProduit() {
-  let produit = await getProduit();
-  console.log(produit);
-  document.querySelector(
-    ".item__img"
-  ).innerHTML = `<img src="${produit.imageUrl}" alt="${produit.altTxt}">`;
-  document.getElementById("title").innerHTML = `${produit.name}`;
-  document.getElementById("price").innerHTML = `${produit.price}`;
-  document.getElementById("description").innerHTML = `${produit.description}`;
-  for (let couleur of produit.colors) {
-    document.getElementById(
-      "colors"
-    ).innerHTML += `<option value="${couleur}">${couleur}</option>`;
-  }
-}
-displayProduit();
+const imgKanap = document.querySelector(".item__img");
+const imgTxtAlt = document.querySelector(".item__img");
+const nomKanap = document.getElementById("title");
+const prixKanap = document.getElementById("price");
+const descKanap = document.getElementById("description");
+const colorOptions = document.getElementById("colors");
+const qteProduit = document.getElementById("quantity");
+fetch(produitUrl)
+  .then((res) => res.json())
+  .then((produit) => {
+    const imgKanape = produit.imageUrl;
+    const imgTxtA = produit.altTxt;
+    const nameKanap = produit.name;
+    const priceKanap = produit.price;
+    const descrKanap = produit.description;
+    const colorsKanap = produit.colors;
+    imgTxtAlt.innerHTML = `<img src="${imgKanape}" alt="${imgTxtA}">`;
+    nomKanap.innerHTML = `${nameKanap}`;
+    prixKanap.innerHTML = `${priceKanap}`;
+    descKanap.innerHTML = `${descrKanap}`;
+    for (let couleur of colorsKanap) {
+      colorOptions.innerHTML += `<option value="${couleur}">${couleur}</option>`;
+    }
+    const ajoutPanier = document.getElementById("addToCart");
+    ajoutPanier.addEventListener("click", () => {
+      let panier = {
+        id: kanapId,
+        name: nameKanap,
+        colors: colorOptions.value,
+        quantity: qteProduit.value,
+        prix: priceKanap,
+      };
+      function recupPanier() {
+        let panier = JSON.parse(localStorage.getItem("kanapLs"));
+        if (panier === null) {
+          return []; //si le LocalStorage est vide, on crée un tableau vide
+        } else {
+          return panier;
+        }
+      }
+      function ajoutPanier(produit) {
+        let panier = recupPanier();
+        const produitIndex = panier.findIndex(
+          (p) => p.id === produit.id && p.colors === produit.colors
+        );
+        if (produitIndex !== -1) {
+          panier[produitIndex].quantity =
+            parseInt(panier[produitIndex].quantity) +
+            parseInt(produit.quantity);
+        } else {
+          produit.quantity = qteProduit.value;
+          panier.push(produit);
+        }
+        sauvPanier(panier);
+        alert(
+          `Le canapé ${nameKanap} ${colorOptions.value} a été ajouté en ${qteProduit.value} exemplaires à votre panier !`
+        );
+      }
+      function sauvPanier(panier) {
+        localStorage.setItem("kanapLs", JSON.stringify(panier));
+      }
+      if (
+        colorOptions.value === "" ||
+        qteProduit.value <= 0 ||
+        qteProduit.value > 100
+      ) {
+        alert(
+          "Veuillez sélectionner une couleur et une quantité valide, merci"
+        );
+      } else if (colorOptions.value === "") {
+        alert("Veuillez sélectionner une couleur, merci");
+      } else if (qteProduit.value <= 0 || qteProduit.value > 100) {
+        alert(
+          "Veuillez sélectionner une quantité supérieure à zéro et inférieure à 100, merci"
+        );
+      } else {
+        ajoutPanier(panier);
+      }
+    });
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
